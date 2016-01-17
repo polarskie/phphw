a7f38a0447ac1e6e8986f85bbe4a959074d04bcd
root <944232369@qq.com>
2016-01-17 19:23:23 +0900

    k

--- addPaper.php
+++ addPaper.php
@@ -85,7 +85,7 @@ if(!isset($_SESSION["studentID"]))
 //            echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
 //            echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
                         move_uploaded_file($_FILES["file"]["tmp_name"],
-                            "upload/" . mysqli_insert_id($con));
+                            "upload/" . mysqli_insert_id($con) . ".pdf");
                         //echo "Stored in: " . "upload/" . mysqli_insert_id($con);
                     }
                 } else {
